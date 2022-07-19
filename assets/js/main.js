0181a1e6629a61727fc764b8bca4f90bd1080f81
juniokun <vinhng74@gmail.com>
2022-07-19 19:44:12 +0700

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,51 @@
+$(document).ready(function(){
+  $('.slickclass').slick({
+    nextArrow: '<div class="arrow-next"><i class="zmdi zmdi-chevron-right"></i></div>',
+    prevArrow: '<div class="arrow-prev"><i class="zmdi zmdi-chevron-left"></i></div>',
+    slidesToShow: 5,
+    slidesToScroll: 5,
+    rows: 1,
+    arrows: false,
+    dots: false,
+    responsive: [
+      {
+        breakpoint: 1440,
+        settings: {
+          slidesToShow: 5,
+          slidesToScroll: 5
+        }
+      },
+      {
+        breakpoint: 1200,
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 4
+        }
+      },
+      {
+        breakpoint: 992,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3
+        }
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3
+        }
+      },
+      {
+        breakpoint: 576,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      }
+    ]
+  });
+});
+
+
+
